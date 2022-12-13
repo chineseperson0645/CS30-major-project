2c7139b279f945f0050587f987764626dd676da6
CompAj <114016409+CompAj@users.noreply.github.com>
2022-12-12 22:21:01 -0600

    Key Pressing Functionality

--- player.js
+++ player.js
@@ -1,162 +1,29 @@
 /* eslint-disable */
 
 class mainPlayer {
-  constructor(runRight, idlePostion, jumpPos, runBack, crouch, jumpPosB, move1,  x, y, playerSpeed, playerHp) {
-    this.x = x; 
-    this.y = y; 
-    this.runR = runRight;
-    this.idleP = idlePostion;
-    this.jumpP = jumpPos;
-    this.runB = runBack;
-    this.crouch = crouch;
-    this.jumpPB = jumpPosB;   
-    this.speed = playerSpeed;
-    this.move1 = move1;  
-    this.hp = playerHp;
-  } 
-
-  runRight () {
-    image(this.runR, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames ==0) {
-      if (frameX < 5) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-    gameFrame++; 
+  constructor({postion, velocity}) {
+    this.postion = postion;
+    this.velocity = velocity;
+    this.height = 150; 
+    this.width = 50;
   }
 
-  idlePostion() {
-    image(this.idleP, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames ==0) {
-      if (frameX < 5) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-   
-    gameFrame++; 
-
+  display() {
+    fill("red"); 
+    rect(this.postion.x, this.postion.y, this.width, this.height); 
   }
 
-  jumpPostion() {
-    image(this.jumpP, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames2 == 0) {
-      if (frameX < 7) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-   
-    gameFrame++; 
-
-  }
-
-
-  runBackwards() {
-    image(this.runB, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames ==0) {
-      if (frameX < 5) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-   
-    gameFrame++; 
-
-  }
-
-  crouchDown() {
-    image(this.crouch, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames ==0) {
-      if (frameX < 8) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-   
-    gameFrame++; 
-
-  }
-
-  jumpPostionB() {
-    image(this.jumpPB, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-    if (gameFrame % staggerFrames2 == 0) {
-      if (frameX < 7) {
-        frameX++; 
-      }
-      else {
-        frameX = 0; 
-      }
-    }
-   
-    gameFrame++; 
-
-  }
-
-
-  move() {
-    //Player Movement Checks 
-    if (keyIsDown(87) === true && keyIsDown(68) === true) { 
-      this.y -= this.speed;
-      this.x += this.speed;
-      this.jumpPostion(); 
-    }
+  update() {
     
-    else if (keyIsDown(87) === true && keyIsDown(65)) { 
-      this.y -= this.speed;
-      this.x -= this.speed;
-      this.jumpPostionB(); 
-    }
+    this.postion.x += this.velocity.x; 
 
-
-
-    else if (keyIsDown(87)) { //w
-      this.y -= this.speed;
-      this.jumpPostion(); 
+    this.postion.y += this.velocity.y;
+    if (this.postion.y + this.height + this.velocity.y >= height) {
+      this.velocity.y = 0; 
     }
-    else if (keyIsDown(83)) { //s
-      this.y += this.speed;
-      this.crouchDown(); 
-    }
-    else if (keyIsDown(68)) { //d
-      this.x += this.speed;
-      this.runRight(); 
-       
-    }
-    else if (keyIsDown(65)) { //a
-      this.x -= this.speed;
-      this.runBackwards(); 
-    }
-    
-    else if (keyIsDown(68) != true && keyIsDown(83) != true && keyIsDown(87) != true &&keyIsDown(65) != true ) {
-      this.idlePostion();
+    else {
+      this.velocity.y += gravity; 
     }
   }
-
-    move1() { 
-      image(this.move1, this.x, this.y, playerWidth, playerHeight, frameX * playerWidth , frameY * playerHeight , playerWidth, playerHeight );
-      if (gameFrame % staggerFrames2 == 0) {
-        if (frameX < 3) {
-          frameX++; 
-        }
-        else {
-          frameX = 0; 
-        }
-      }
-     
-      gameFrame++; 
-  
-    }
 }
-
+  
--- sketch.js
+++ sketch.js
@@ -1,66 +1,145 @@
 // CS30 Major Project
 // Anjana Samarasinghe
 
+const gravity = 0.2;
 
-let runRight;
-let runBack; 
-let idlePos;
-let jumpPos;
-let jumpPosB;  
-let crouch; 
-let playerHeight; 
-let playerWidth; 
-let frameX; 
-let frameY; 
+const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  w: {
+    pressed: false
+  }
+}
+
+let lastkey;
+
+// let runRight;
+// let runBack; 
+// let idlePos;
+// let jumpPos;
+// let jumpPosB;  
+// let crouch; 
+// let playerHeight; 
+// let playerWidth; 
+// let frameX; 
+// let frameY; 
 let player1;
-let gameFrame;
-let staggerFrames;
-let staggerFrames2;
-let bgImage;
-let bgImage2;
-let timer;
-let move1; 
+// let gameFrame;
+// let staggerFrames;
+// let staggerFrames2;
+// let bgImage;
+// let bgImage2;
+// let timer;
+// let move1;
+let enmey1; 
 
 
 
 function preload() {
-  runRight = loadImage("assets/Run.png");
-  idlePos = loadImage("assets/Idle.png");
-  jumpPos = loadImage("assets/Jump.png");
-  runBack = loadImage("assets/runB.png");
-  crouch = loadImage("assets/Disguise.png");
-  jumpPosB = loadImage("assets/jumpB.png"); 
-  bgImage = loadImage("assets/moutian-pixel.gif");
-  bgImage2 = loadImage("assets/city-pixle.gif");
-  move1 = loadImage("assets/Attack_2.png"); 
+  // runRight = loadImage("assets/Run.png");
+  // idlePos = loadImage("assets/Idle.png");
+  // jumpPos = loadImage("assets/Jump.png");
+  // runBack = loadImage("assets/runB.png");
+  // crouch = loadImage("assets/Disguise.png");
+  // jumpPosB = loadImage("assets/jumpB.png"); 
+  // bgImage = loadImage("assets/moutian-pixel.gif");
+  // bgImage2 = loadImage("assets/city-pixle.gif");
+  // move1 = loadImage("assets/Attack_2.png"); 
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  playerWidth = 96; 
-  playerHeight = 96; 
-  frameX = 0; 
-  frameY = 0;
-  gameFrame = 0;
-  staggerFrames = 7;
-  staggerFrames2 = 3; 
-  player1 = new mainPlayer(runRight,idlePos, jumpPos,runBack, crouch, jumpPosB, move1, 0,0,5,100); 
-  timer = 0;
-  move1 = true; 
+  // playerWidth = 96; 
+  // playerHeight = 96; 
+  // frameX = 0; 
+  // frameY = 0;
+  // gameFrame = 0;
+  // staggerFrames = 7;
+  // staggerFrames2 = 3; 
+  ////Player Stuff////
+  
+  player1 = new mainPlayer({
+    postion: {
+    x: 0,
+    y: 0
+  },
+  velocity: {
+    x: 0, 
+    y: 0
+  }
+})
+
+  enmey1 = new mainPlayer({
+    postion: {
+    x: 400,
+    y: 100
+  },
+  velocity: {
+    x: 0, 
+    y: 0
+  }
+})
+
+
+
 }
 
 function draw() {
   background(220);
-  image(bgImage, 0, 0, width, height);
-  player1.move();
-   
+  player1.update();
+  enmey1.update();
+  player1.display();
+  enmey1.display();
+
+  player1.velocity.x = 0;
+
+
+  if (keys.a.pressed === true && lastkey === "a") {
+    player1.velocity.x = -1; 
+  }
+  else if (keys.d.pressed === true && lastkey === "d"){
+    player1.velocity.x = 1; 
+  }
 }
 
-function cooldown1() { 
-  if (timer === 0) { 
-    timer = true; 
+window.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = true;
+      lastkey = "d"; 
+      break
+      
+    case 'a':
+       keys.a.pressed = true;
+       lastkey = "a"; 
+      break
+      
+    case 'w':
+      player1.velocity.y = -10; 
+    break 
   }
-  else {
-    timer = false;
-  }  
-}
+  console.log(event.key); 
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'd':
+      keys.d.pressed = false; 
+      break 
+      
+    case 'a':
+      keys.a.pressed = false; 
+      break
+    
+    case 'w':
+      keys.w.pressed = false; 
+      break
+  }
+
+  console.log(event.key); 
+})
+
