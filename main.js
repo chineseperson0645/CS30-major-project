4bd66191f0cced3b9c1862bd1e26a187914c831f
ilovemath912 <114017596+chineseperson0645@users.noreply.github.com>
2022-12-14 18:06:44 -0600

    Update main.js

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ let cellHeight;
 let playerX = 0;
 let playerY = 0;
 
-let onTree = false;
+let nearTree = false;
 
 let grassImg, rockImg, horseImg;
 
@@ -31,7 +31,7 @@ function setup() {
   cellHeight = height/ROWS;
   grid = create2dArray(COLS, ROWS);
   //place player in grid
-  grid[playerY+1][playerX+1] = 9;
+  grid[playerY][playerX] = 9;
 }
 
 function draw() {
@@ -40,70 +40,75 @@ function draw() {
   surroundingCheck();
 }
 
+// function allBlocks(){
+//   grid[playerY][playerX] = 0;
+// }
+
 //Allows Horse to pass through (and draw over) a image
 function keyPressed() {
-  if (keyCode === RIGHT_ARROW) {
-  //SO IF this spot is blank (0) or a tree (2) update the player location (playerX++) draw in that direction with a horse
+  if (key === "d") {
+  //SO IF this spot is blank (0) update the player location (playerX++) draw in that direction with a horse
     if (grid[playerY][playerX+1] === 0) {
-      grid[playerY][playerX] = 0; //reset old location to white
-      playerX++; //move
-      grid[playerY][playerX] = 9; //set new player location
+        grid[playerY][playerX] = 0; //reset old location to white
+        playerX++; //move
+        grid[playerY][playerX] = 9; //set new player location
       }
     }
-    else if (onTree) {
+    else if (grid[playerY][playerX+1] === 0 && nearTree) {
       grid[playerY][playerX] = 0; //reset old location to white
       playerX++; //move
       grid[playerY][playerX] = 10; //set new player location
-      onTree = false;
+      nearTree = false;
       }
 
-  if (keyCode === LEFT_ARROW) {
+  if (key === "a") {
     if (grid[playerY][playerX-1] === 0) {
-      grid[playerY][playerX] = 0; //reset old location to white
-      playerX--; //move
-      grid[playerY][playerX] = 9; //set new player location
+        grid[playerY][playerX] = 0; //reset old location to white
+        playerX--; //move
+        grid[playerY][playerX] = 9; //set new player location
     }
-    else if (onTree) {
+    else if (grid[playerY][playerX-1] === 0 && nearTree) {
       grid[playerY][playerX] = 0; //reset old location to white
       playerX--; //move
       grid[playerY][playerX] = 10; //set new player location
-      onTree = false;
+      nearTree = false;
     }
   }
 
-  if (keyCode === UP_ARROW) {
+  if (key === "w") {
     if (grid[playerY-1][playerX] === 0) {
-      grid[playerY][playerX] = 0; //reset old location to white
-      playerY--; //move
-      grid[playerY][playerX] = 9; //set new player location
+        grid[playerY][playerX] = 0; //reset old location to white
+        playerY--; //move
+        grid[playerY][playerX] = 9; //set new player location
     }
-    else if (onTree) {
+    else if (grid[playerY-1][playerX] === 0 && nearTree) {
       grid[playerY][playerX] = 0; //reset old location to white
       playerY--; //move
       grid[playerY][playerX] = 10; //set new player location
-      onTree = false;
+      nearTree = false;
     }
   }
 
-  if (keyCode === DOWN_ARROW) {
+  if (key === "s") {
     if (grid[playerY+1][playerX] === 0) {
-      grid[playerY][playerX] = 0; //reset old location to white
-      playerY++; //move
-      grid[playerY][playerX] = 9; //set new player location
-    }
-    else if (onTree) {
+        grid[playerY][playerX] = 0; //reset old location to white
+        playerY++; //move
+        grid[playerY][playerX] = 9; //set new player location
+    } 
+    else if (grid[playerY+1][playerX] === 0 && nearTree) {
       grid[playerY][playerX] = 0; //reset old location to white
       playerY++; //move
       grid[playerY][playerX] = 10; //set new player location
-      onTree = false;
+      nearTree = false;
     }
   }
 }
+
+//Current problem is that it detects it nearTree (or near one) and adds the playerX or Y twice because it's also moving from whitespace.
 //If moved into a certain grid position. Player X and Y are still tracked.
-//But if onTree is true. Draw a 10 instead of a 9. (10 can just be a tree ontop of a tree)
+//But if nearTree is true. Draw a 10 instead of a 9. (10 can just be a tree ontop of a tree)
 
 function displayGrid(grid) {
-
   for (let y=0; y<ROWS; y++) {
     for (let x=0; x<COLS; x++) {
 
@@ -131,11 +136,8 @@ function displayGrid(grid) {
 
 function surroundingCheck(){
   //  Down Arrow                        Left Arrow                        Right Arrow
-  if (grid[playerY+1][playerX] === 2 || grid[playerY][playerX-1] === 2 || grid[playerY][playerX+1] === 2){
-    onTree = true;
-  }
-  else if (grid[playerY+1][playerX] === 0 || grid[playerY][playerX-1] === 0 || grid[playerY][playerX+1] === 0){
-    onTree
+  if (grid[playerY+1][playerX] === 2 || grid[playerY][playerX-1] === 2 || grid[playerY][playerX+1] === 2 || grid[playerY-1][playerX] === 2){
+    nearTree = true;
   }
 }
 
