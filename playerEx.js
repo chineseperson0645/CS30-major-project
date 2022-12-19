798930648c87ac4e19e74e97d66029f7d7ae8475
CompAj <114016409+CompAj@users.noreply.github.com>
2022-12-19 12:17:43 -0600

    Added gravity to second class

--- playerEx.js
+++ playerEx.js
@@ -13,11 +13,31 @@ class Player {
 
   update() {
 
+    this.postion.x += this.velocity.x; 
+    this.postion.y += this.velocity.y;
+
     if (this.postion.y + this.height + this.velocity.y >= height) {
       this.velocity.y = 0; 
     }
     else {
-      this.velocity.y += gravity; 
+      this.velocity.y += gravity;
     }
   }
+
+  // movement(){
+  //   window.addEventListener('keyup', (event) => {
+  //     switch (event.key) {
+  //       case 'ArrowRight':
+  //     keys.ArrowRight.pressed = false;
+  //     break
+    
+  //   case 'ArrowLeft':
+  //     keys.ArrowLeft.pressed = false;
+  //     break
+    
+  //   case 'ArrowUp':
+  //     keys.ArrowUp.pressed = false;
+  //     break
+  //     }
+  // }
 }
