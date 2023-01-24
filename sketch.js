61cea947574046879a81f6be3f5198e44e24a862
CompAj <114016409+CompAj@users.noreply.github.com>
2023-01-23 20:27:26 -0600

    Adding Health and Pushing to Main Branch

--- sketch.js
+++ sketch.js
@@ -297,3 +297,8 @@ function getDistance(x1,y1,x2,y2) {
 function rectCol({rectangle1,rectangle2}) {
   return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height); 
 }
+
+function healthBar(health, maxHealth) {
+  stroke(0); 
+  stokeWeight(4); 
+}
