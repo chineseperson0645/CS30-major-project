2488a1ad18abe99e98d5a86d140913544f5519d1
CompAj <114016409+CompAj@users.noreply.github.com>
2023-01-24 17:02:23 -0600

    Adding Comments

--- sketch.js
+++ sketch.js
@@ -1,7 +1,10 @@
 // CS30 Major Project
-// Anjana Samarasinghe
+// Anjana Samarasinghe & Micheal Gao
+
+//Gravity Affect the y velocity of all entities
 const gravity = 0.7;
 
+//Becomes true when key is the specified key is pressed
 const keys = {
   a: {
     pressed: false
@@ -14,6 +17,7 @@ const keys = {
   }
 };
 
+//Variales
 let playerEx; 
 let runRight;
 let runBack; 
@@ -37,6 +41,7 @@ let enemyHealth;
 let attackInterval = 1000; 
 let lastAttackTime = 0;
 
+//Loads images before game starts
 function preload() {
   //Player Sprite Sheets
   runRight = loadImage("assets/Run.png");
@@ -74,15 +79,19 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  
+  //controls what point we are in the game and activates all main code
   state = "fight"; 
   
+  //Player Health
   globalPlayerHealth = 100; 
   
+  //Max ammount the player health can go to
   maxHP = 100;
   
+  //Max ammount the enemy health can go to
   enemyHealth = 200; 
   
+  //Making the an player using a class
   playerEx = new Player({
     velocity: {
       x: 0,
@@ -146,7 +155,7 @@ function setup() {
   });
 
 
-  
+  //Making the an enemy using a class
   shinso = new Enemy({
     velocity: {
       x: 0,
@@ -211,16 +220,19 @@ function setup() {
 
 function draw() {
 
-  
+  //Runs code if state is equal to "fight"
   if (state === "fight") {
   background(220);
+
+  //Background Gif
   image(bgImage, 0, 0, width, height);
   
-    console.log(shinso.health); 
+
   
     // Check if the enemy or player has died
     if (shinso.health <= 0) { //enemy death
       shinso.switchanimation("death");
+      
     }
 
     if (playerEx.health <= 0) {//player death
@@ -247,17 +259,15 @@ function draw() {
     //Enemy Movement
     if (shinso.health > 0 && shinso.dead === false) {
       if (getDistance(playerEx.position.x, playerEx.position.y, shinso.position.x,shinso.position.y) < 40) {
-        // shinso.attack();
-
+        
+        //Times enemy attack so the attack button would not be rapidly triggered
         if (millis() - lastAttackTime >= attackInterval) {
           shinso.attack();
           lastAttackTime = millis();
       }
-        
-        
       }
       
-
+      //Checks where player is and moves towards them slowly
       if (playerEx.position.x <= shinso.position.x) {
         shinso.position.x -= 3;
         shinso.switchanimation('runback');
@@ -268,17 +278,14 @@ function draw() {
         shinso.switchanimation('run');
     }
   }
-    //
 
-    // console.log(getDistance(playerEx.position.x, playerEx.position.y, shinso.position.x,shinso.position.y));
 
     
 
-
+    //Moves player left
     if (keys.a.pressed === true && lastKeys === "a") {
       playerEx.velocity.x = -5;
       playerEx.switchSprite('runback');
-    
     }
     else if (keys.d.pressed === true && lastKeys === "d"){
       playerEx.velocity.x = 5;
@@ -295,32 +302,22 @@ function draw() {
     if (playerEx.velocity.y < 0) {
       playerEx.switchSprite('Up'); 
     }
-
+    //Fall animation trigger
     else if (playerEx.velocity.y > 0) {
       playerEx.switchSprite('Down'); 
     }
     
-    // if (shinso.velocity.y < 0) {
-    //   shinso.switchanimation('Up'); 
-    // }
-    // else if (shinso.velocity.y > 0) {
-    //   shinso.switchanimation('Down'); 
-    // }
+   
 
 
-    // collision detection
+    // collision detection & damage detection
   if (rectCol({rectangle1: playerEx, rectangle2: shinso}) && playerEx.isAttacking) {
     playerEx.isAttacking = false; 
-    // console.log("detection");
-    // enemyHealth -= 10;
-    // shinso.switchanimation('hit');
     shinso.takeHit(); 
   }
 
   if (rectCol({rectangle1: shinso, rectangle2: playerEx}) && shinso.isAttacking) {
     shinso.isAttacking = false;
-    // console.log("hazaa");
-    // playerEx.switchSprite('hit');
     playerEx.takeHit(); 
 
     }
@@ -328,7 +325,6 @@ function draw() {
 }
   
 //Movement
-  
 window.addEventListener('keydown', (event) => {
 
   switch (event.key) {
@@ -375,6 +371,7 @@ window.addEventListener('keyup', (event) => {
   }
 });
 
+//gets the distance
 function getDistance(x1,y1,x2,y2) {
    xdiff = x1-x2;
    ydiff = y1-y2; 
@@ -382,13 +379,13 @@ function getDistance(x1,y1,x2,y2) {
    return Math.sqrt(xdiff * xdiff + ydiff * ydiff); 
 }
 
-
+//Detectes where the proper collision should happen
 function rectCol({rectangle1,rectangle2}) {
   return (rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y && rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height); 
 }
 
 
-
+//Creates health bar and allows you to place it anywhere
 function healthBar(health, maxHealth, xOffset, widthOffset) {
   stroke(0); 
   strokeWeight(7); 
